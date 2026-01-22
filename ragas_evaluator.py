9f485d33551c32af6cf8ea43b22f5c5609bb7387
Sascha Krumbach <sascha.s.krumbach@gmail.com>
2026-01-21 21:35:23 -0500

    stage

--- ragas_evaluator.py
+++ ragas_evaluator.py
@@ -3,11 +3,12 @@ from ragas.embeddings import LangchainEmbeddingsWrapper
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from typing import Dict, List, Optional
-
+import os
 # RAGAS imports
 try:
     from ragas import SingleTurnSample
-    from ragas.metrics import BleuScore, NonLLMContextPrecisionWithReference, ResponseRelevancy, Faithfulness, RougeScore
+    from ragas.metrics.collections import BleuScore, Faithfulness, RougeScore
+    from ragas.metrics import NonLLMContextPrecisionWithReference, ResponseRelevancy,
     from ragas import evaluate
     RAGAS_AVAILABLE = True
 except ImportError:
@@ -17,9 +18,22 @@ def evaluate_response_quality(question: str, answer: str, contexts: List[str]) -
     """Evaluate response quality using RAGAS metrics"""
     if not RAGAS_AVAILABLE:
         return {"error": "RAGAS not available"}
-    
+    openai_api_key = os.environ("OPENAI_API_KEY")
     # TODO: Create evaluator LLM with model gpt-3.5-turbo
+    evaluator_llm = LangchainLLMWrapper(ChatOpenAI(
+            model="gpt-3.5-turbo",
+            api_key=openai_api_key,
+            # base_url="https://openai.vocareum.com/v1"
+        ))
     # TODO: Create evaluator_embeddings with model test-embedding-3-small
+    langchain_openai_embeddings = OpenAIEmbeddings(
+            api_key=openai_api_key,
+            model="test-embedding-3-small"
+            # openai_api_base="https://openai.vocareum.com/v1"
+        )
+    evaluator_embeddings = LangchainEmbeddingsWrapper(langchain_openai_embeddings)
+
+
     # TODO: Define an instance for each metric to evaluate
     # TODO: Evaluate the response using the metrics
     # TODO: Return the evaluation results
